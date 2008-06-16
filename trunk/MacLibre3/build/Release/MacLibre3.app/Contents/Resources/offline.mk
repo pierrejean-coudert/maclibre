33d12dddba841af08d85d14d8a89f714c6d493ff
ezberch <ezberch@6caf0352-ff4e-0410-ad89-e35968282056>
2008-06-16 22:05:26 +0000

    fixing Error: 155005

--- trunk/MacLibre3/build/Release/MacLibre3.app/Contents/Resources/offline.mk
+++
@@ -1,9 +0,0 @@
-################################################################################
-# Offline distribution variables
-################################################################################
-
-NAME=MacLibreOffline
-TEMPLATE_DMG=../../bin/MacLibreOfflineTemplate.dmg
-VERSION=0.2
-SOURCE_DIR=../dist
-SOURCE_FILES=MacLibre.app
