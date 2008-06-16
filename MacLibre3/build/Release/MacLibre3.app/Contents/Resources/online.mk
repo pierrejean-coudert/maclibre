1b69d04a1a009bbbe03b5a870ff5ddcb62975688
ezberch <ezberch@6caf0352-ff4e-0410-ad89-e35968282056>
2008-06-16 22:05:26 +0000

    fixing Error: 155005

--- MacLibre3/build/Release/MacLibre3.app/Contents/Resources/online.mk
+++
@@ -1,9 +0,0 @@
-################################################################################
-# Online distribution variables
-################################################################################
-
-NAME=MacLibre
-TEMPLATE_DMG=../../bin/MacLibreTemplate.dmg
-VERSION=0.2
-SOURCE_DIR=../dist
-SOURCE_FILES=MacLibre.app
